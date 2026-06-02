0a2a9451239b1963d33974222fa93a06d703fecf
Alejandro <alexnachovc12@gmail.com>
2026-06-01 23:36:33 -0600

    fix: cors filter with higher priority than auth

--- src/main/java/com/edutask/interfaces/rest/CorsFilter.java
+++ src/main/java/com/edutask/interfaces/rest/CorsFilter.java
@@ -1,12 +1,34 @@
 package com.edutask.interfaces.rest;
 
+import jakarta.annotation.Priority;
+import jakarta.ws.rs.Priorities;
 import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerRequestFilter;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
 
 @Provider
-public class CorsFilter implements ContainerResponseFilter {
+@Priority(Priorities.AUTHENTICATION - 100)
+public class CorsFilter implements ContainerRequestFilter, ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) {
+        // Manejar preflight OPTIONS antes de auth
+        if (requestContext.getMethod().equalsIgnoreCase("OPTIONS")) {
+            requestContext.abortWith(
+                    Response.ok()
+                            .header("Access-Control-Allow-Origin", "*")
+                            .header("Access-Control-Allow-Headers",
+                                    "origin, content-type, accept, authorization")
+                            .header("Access-Control-Allow-Methods",
+                                    "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+                            .header("Access-Control-Max-Age", "1209600")
+                            .build()
+            );
+        }
+    }
 
     @Override
     public void filter(ContainerRequestContext requestContext,
